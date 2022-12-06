38c8eab1ba54f80c6a41b60a127344be4983bd22
soniapop93 <popsonia85@gmail.com>
2022-12-07 00:58:46 +0200

    Day 3, first part is done. I didn't start the second part yet

---
+++ Day_3/day_3_1.py
@@ -0,0 +1,33 @@
+input_list = []
+
+with open("input.txt", "r+") as f:
+    input_list = [line.replace("\n", "") for line in f]
+
+print(input_list)
+
+
+def sum_of_priorities_items(input_list):
+    lowercase_letters = [chr(lower_letter) for lower_letter in range(ord('a'), ord('z') + 1)]
+    uppercase_letters = [chr(upper_letter) for upper_letter in range(ord('A'), ord('Z') + 1)]
+
+    combined_letters = lowercase_letters + uppercase_letters
+
+    final_sum = 0
+    for item in input_list:
+        common_item = ""
+        first_compartment = item[:int(len(item) / 2)]
+        second_compartment = item[int(len(item) / 2):]
+
+        for i in first_compartment:
+            for j in second_compartment:
+                if i in j:
+                    common_item = i
+                    break
+
+        if common_item in combined_letters:
+            final_sum = final_sum + (int(combined_letters.index(common_item) + 1))
+
+    return final_sum
+
+
+print(sum_of_priorities_items(input_list))
